6019f18b54ecda3e3b49c22e23cad172962089de
Phineas Ziegler <phinziegler@gmail.com>
2021-12-18 02:24:20 -0600

    added files

---
+++ src/script.js
@@ -0,0 +1,12 @@
+console.log("loaded");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
+function drawBoard(rows, columns, squareTiles=true) {
+    for (let i = 0; i < rows; i++) {
+        
+    }
+
+}
