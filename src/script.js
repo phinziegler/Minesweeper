5beb1046e625a4b139c179d9b2e6b968dc44ef91
Phineas Ziegler <phinziegler@gmail.com>
2021-12-18 18:58:42 -0600

    styling and future prep

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import Board from "./board.js";
 
 const canvas = document.getElementById("canvas");
-const board = new Board(canvas, 20, 20, "rgb(100, 100, 100)", "rgb(150, 150, 150)");
+const board = new Board(canvas, 10, 10, "rgb(100, 100, 100)", "rgb(150, 150, 150)");
 board.draw();
 
