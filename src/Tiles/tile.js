bbe421170d0220c2e722aea62b31e8ed7d61de24
Phineas Ziegler <phinziegler@gmail.com>
2021-12-18 19:44:33 -0600

    3d look to unsolved tiles

--- src/Tiles/tile.js
+++ src/Tiles/tile.js
@@ -7,13 +7,61 @@ export default class Tile {
             y: y,
         }
         this.color = color;
+        this.sidelength = sidelength;
         this.width = sidelength;
         this.height = sidelength;
+        this.isSolved = false;
     }
     draw() {
-        this.ctx.fillStyle = this.color;
-        this.ctx.fillRect(this.position.x, this.position.y, this.width, this.height); // x y width height
+        const x = this.position.x;
+        const y = this.position.y;
+        let ctx = this.ctx;
+
+        ctx.fillStyle = this.color;
+        ctx.fillRect(x, y, this.width, this.height); // x y width height
+        
+        if(!this.isSolved) {
+            const indentDepth = .333;
+            this.drawIndentLight(ctx, x, y, 2 / (indentDepth), "rgba(255, 255, 255, 0.6)");
+            this.drawIndentDark(ctx, x, y, 2 / (indentDepth), "rgba(0, 0, 0, 0.33)");
+        }
     }
+
+    drawIndentLight(ctx, x, y, fac, color) {
+        const len = this.sidelength;
+        const maxX = x + len;
+        const maxY = y + len;
+        const val = Math.floor(len / fac);
+
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(maxX, y);
+        ctx.lineTo(maxX - val, y + val);
+        ctx.lineTo(x + val, y + val);
+        ctx.lineTo(x + val, maxY - val);
+        ctx.lineTo(x, maxY);
+        ctx.closePath();
+        ctx.fill();
+    }
+    drawIndentDark(ctx, x, y, fac, color) {
+        const len = this.sidelength;
+        const maxX = x + len;
+        const maxY = y + len;
+        const val = Math.floor(len / fac);
+
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(maxX, maxY);
+        ctx.lineTo(x, maxY);
+        ctx.lineTo(x + val, maxY - val);
+        ctx.lineTo(maxX - val, maxY - val);
+        ctx.lineTo(maxX - val, y + val);
+        ctx.lineTo(maxX, y);
+        ctx.closePath();
+        ctx.fill();
+    }
+
     handleClick() {
         throw new Error("handleClick() cannot be called on abstract class \"Tile\"");
     }
