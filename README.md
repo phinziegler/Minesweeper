08eb2c1f8a0ab7aa8d8e116648469b6daf450183
Phineas Ziegler <phinziegler@gmail.com>
2021-12-19 03:15:56 -0600

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Minesweeper
  Fully front end minesweeper clone
+
+ ## Goals
+ Interface to change game dimensions and board size.
+ When you click a bomb, the board shatters into physics objects.
--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import Board from "./board.js";
 
 const canvas = document.getElementById("canvas");
-const board = new Board(canvas, 10, 20, "rgb(150, 150, 150)", "rgb(150, 150, 150)");
+const board = new Board(canvas, 16, 16, "rgb(150, 150, 150)", "rgb(150, 150, 150)");
 board.draw();
-board.createBombs(1);
+board.createBombs(50);
 
