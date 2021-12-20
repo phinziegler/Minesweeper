5ac5d62d5bf46c3daae75f34d5057d97c75d21da
Phineas Ziegler <phinziegler@gmail.com>
2021-12-19 19:12:37 -0600

    im just praying it will work

--- src/Tiles/bombTile.js
+++ src/Tiles/bombTile.js
@@ -12,21 +12,21 @@ export default class BombTile extends Tile {
         this.isSolved = true;
     }
     drawData(ctx, x, y, color) {
-        // let image = document.getElementById("mine");
+        let image = document.getElementById("mine");
         this.drawFill(ctx, x, y, "rgba(220, 50, 50, 1)");
 
-        let c2 = document.getElementById("c2");
-        let c2ctx = c2.getContext("2d");
+        // let c2 = document.getElementById("c2");
+        // let c2ctx = c2.getContext("2d");
 
-        let image = new Image();
-        image.onload = function() {
-            console.log("load");
+        // let image = new Image();
+        // image.onload = function() {
+            // console.log("load");
             let width = this.sidelength / 1.3;
             x = x + ((this.sidelength - width) / 2);
             y = y + ((this.sidelength - width) / 2);
-            c2ctx.drawImage(image, x, y, width, width);
-            console.log("done");
-        };
-        image.src = "/src/images/mine.png";
+            ctx.drawImage(image, x, y, width, width);
+            // console.log("done");
+        // };
+        // image.src = "/src/images/mine.png";
     }
 }
