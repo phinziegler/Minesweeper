2c2caa32a394de5cca17e48ba9e2ddeb13b53939
Phineas Ziegler <phinziegler@gmail.com>
2021-12-19 18:35:03 -0600

    bro

--- src/Tiles/bombTile.js
+++ src/Tiles/bombTile.js
@@ -13,15 +13,15 @@ export default class BombTile extends Tile {
     }
     drawData(ctx, x, y, color) {
         // let image = document.getElementById("mine");
-        let image = new Image();
-        image.src = "src/images/mine.png";
         this.drawFill(ctx, x, y, "rgba(220, 50, 50, 1)");
-
-        let width = this.sidelength / 1.3;
-        x = x + ((this.sidelength - width) / 2);
-        y = y + ((this.sidelength - width) / 2);
-
-        ctx.drawImage(image, x, y, width, width);
-
+        let image = new Image();
+        image.src = "mine.png";
+        image.onload = function() {
+            console.log("load");
+            let width = this.sidelength / 1.3;
+            x = x + ((this.sidelength - width) / 2);
+            y = y + ((this.sidelength - width) / 2);
+            ctx.drawImage(image, x, y, width, width);
+        };
     }
 }
