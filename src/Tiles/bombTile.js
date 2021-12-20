852f9a34bf0073e849779b6c554c23c7fe358659
Phineas Ziegler <phinziegler@gmail.com>
2021-12-19 18:39:07 -0600

    maybe pages will load?

--- src/Tiles/bombTile.js
+++ src/Tiles/bombTile.js
@@ -12,16 +12,16 @@ export default class BombTile extends Tile {
         this.isSolved = true;
     }
     drawData(ctx, x, y, color) {
-        // let image = document.getElementById("mine");
+        let image = document.getElementById("mine");
         this.drawFill(ctx, x, y, "rgba(220, 50, 50, 1)");
-        let image = new Image();
-        image.src = "mine.png";
-        image.onload = function() {
+        // let image = new Image();
+        // image.src = "/src/images/mine.png";
+        // image.onload = function() {
             console.log("load");
             let width = this.sidelength / 1.3;
             x = x + ((this.sidelength - width) / 2);
             y = y + ((this.sidelength - width) / 2);
             ctx.drawImage(image, x, y, width, width);
-        };
+        // };
     }
 }
