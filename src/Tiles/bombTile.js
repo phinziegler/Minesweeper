2499c3820bb11a57e787c3a86ed9f5769acfa22f
Phineas Ziegler <phinziegler@gmail.com>
2021-12-21 13:43:41 -0600

    added circle draw on shine for mines

--- src/Tiles/bombTile.js
+++ src/Tiles/bombTile.js
@@ -14,9 +14,18 @@ export default class BombTile extends Tile {
     drawData(ctx, x, y, color) {
         let image = document.getElementById("mine");
         this.drawFill(ctx, x, y, "rgba(220, 50, 50, 1)");
+
+        this.drawCircle(ctx, x, y, "rgba(255,255,255, .6)");
+
         let width = this.sidelength / 1.15;
         x = x + ((this.sidelength - width) / 2);
         y = y + ((this.sidelength - width) / 2);
         ctx.drawImage(image, x, y, width, width);
+    }
+    drawCircle(ctx, x, y, color) {
+        ctx.beginPath();
+        ctx.arc(x + (this.sidelength / 2.8), y + (this.sidelength / 2.5), this.sidelength / 10, 0, 2 * Math.PI, false);
+        ctx.fillStyle = color;
+        ctx.fill();
     }
 }
